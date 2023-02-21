134b05d9cf6795907c3d5fea34ef53cbe7c05755
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-02-22 00:57:28 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# navigate to the directory containing the Python script
+cd /path/to/bot/directory
+
+# activate the virtual environment (if using one)
+source /path/to/virtual/environment/bin/activate
+
+# start the Python script
+python3 bot.py
